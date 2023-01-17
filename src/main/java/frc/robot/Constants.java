51c8e042a58abd2aa32797b1fee0f6effe5de25a
GeoF0945 <86170124+GeoF0945@users.noreply.github.com>
2023-01-17 17:46:21 -0500

    swerve working

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,9 +31,9 @@ public final class Constants {
     public static final double kMaxAngularSpeed = 2 * Math.PI; // radians per second
 
     // Chassis configuration
-    public static final double kTrackWidth = Units.inchesToMeters(26.5);;
+    public static final double kTrackWidth = Units.inchesToMeters(20.5);;
     // Distance between centers of right and left wheels on robot
-    public static final double kWheelBase = Units.inchesToMeters(26.5);;
+    public static final double kWheelBase = Units.inchesToMeters(24.5);;
     // Distance between front and back wheels on robot
     public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
         new Translation2d(kWheelBase / 2, kTrackWidth / 2),
@@ -48,15 +48,15 @@ public final class Constants {
     public static final double kBackRightChassisAngularOffset = Math.PI / 2;
 
     // SPARK MAX CAN IDs
-    public static final int kFrontLeftDrivingCanId = 11;
-    public static final int kRearLeftDrivingCanId = 13;
-    public static final int kFrontRightDrivingCanId = 15;
-    public static final int kRearRightDrivingCanId = 17;
+    public static final int kFrontLeftDrivingCanId = 1;
+    public static final int kRearLeftDrivingCanId = 3;
+    public static final int kFrontRightDrivingCanId = 7;
+    public static final int kRearRightDrivingCanId = 5;
 
-    public static final int kFrontLeftTurningCanId = 10;
-    public static final int kRearLeftTurningCanId = 12;
-    public static final int kFrontRightTurningCanId = 14;
-    public static final int kRearRightTurningCanId = 16;
+    public static final int kFrontLeftTurningCanId = 2;
+    public static final int kRearLeftTurningCanId = 4;
+    public static final int kFrontRightTurningCanId = 8;
+    public static final int kRearRightTurningCanId = 6;
 
     public static final boolean kGyroReversed = false;
   }
@@ -106,7 +106,7 @@ public final class Constants {
   }
 
   public static final class OIConstants {
-    public static final int kDriverControllerPort = 0;
+    public static final int kDriverControllerPort = 5;
   }
 
   public static final class AutoConstants {
@@ -125,12 +125,12 @@ public final class Constants {
   }
 
   public static final class IntakeConstants {
-    public static final int kIntakePort = 1;
+    public static final int kIntakePort = 11;
     public static final double kIntakeSpeed = 1;
   }
 
   public static final class ElevatorConstants {
-    public static final int[] kElevatorPorts = {3,4};
+    public static final int[] kElevatorPorts = {9,10};
     public static final double kElevatorSpeed = 1;
   }
 }
